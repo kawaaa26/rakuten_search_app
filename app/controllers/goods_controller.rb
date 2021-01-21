f4ac57f094280078a6f2a36ae0f78c28c5e58b75
kawaaa26 <unic.almj@yahoo.com>
2021-01-21 19:38:42 +0900

    :fire: removed binding.irb from goods_controller.rb

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -2,7 +2,6 @@ class GoodsController < ApplicationController
   def search
     if params[:keyword]
       @items = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword])
-      # binding.irb
     end
   end
 end
